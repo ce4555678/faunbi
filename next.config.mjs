04b0b5f0b5322e903cf8e7483af2ab65497b6aa7
Carlos Eduardo <ce4555678@gmail.com>
2026-05-18 20:34:19 -0300

    workflow upstash

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    serverExternalPackages: ['ppu-paddle-ocr', '@napi-rs/canvas'],
 }
 
 export default nextConfig
--- src/app/api/image-ocr/route.ts
+++ src/app/api/image-ocr/route.ts
@@ -1,18 +1,13 @@
-import { PaddleOcrService } from "ppu-paddle-ocr"
+import Ocr from '@gutenye/ocr-node'
 
 export const runtime = "nodejs"
 export const GET = async (request: Request) => {
   const file = await fetch("https://tesseract.projectnaptha.com/img/eng_bw.png")
   const image = await file.blob()
+  const ocr = await Ocr.create()
+const result = await ocr.detect(await image.arrayBuffer())
   // 1. Converte o Blob para ArrayBuffer
-  const arrayBuffer = await image.arrayBuffer()
 
-  const service = new PaddleOcrService()
-  await service.initialize()
-
-  const result = await service.recognize(arrayBuffer, {
-    strategy: "per-box",
-  })
 
   return Response.json({
     text: result.text,
