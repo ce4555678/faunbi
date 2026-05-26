64440ef23b83d98fe4dcc708449d71bada1b5226
Carlos Eduardo <ce4555678@gmail.com>
2026-05-26 20:10:36 -0300

    corrigido

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,7 @@ import { drizzle } from "drizzle-orm/libsql/http"
 export default async function connectDB(userId: string) {
   const db = drizzle({
     connection: {
-      url: `libsql://${userId}-ce4555678.aws-us-east-1.turso.io`,
+      url: `libsql://${userId.toLowerCase()}-ce4555678.aws-us-east-1.turso.io`,
       authToken: process.env.TURSO_AUTH_TOKEN,
     },
   })
